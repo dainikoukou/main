75a9d64a31c87e6add1e5ccaab63b40cd33d5730
dainikoukou <dainikoukou@gmail.com>
2018-01-27 23:05:14 +0900

    javaを入れてみてる（コピペ編）

--- www/js/main.js
+++ www/js/main.js
@@ -1,9 +1,27 @@
 // This is a JavaScript file
 
-function save(){
-    alert('hello world!'); 
+function save(list){
+    var list = getMemoList();
+    var time = new Date().getTime();
+    list.push({ id: time, time: time, text: text });
+    saveMemoList(list);
+    try {
+        localStorage.setItem("memo_list", JSON.stringify(list));
+    } catch (e) {
+        alert('Error saving to storage.');
+        throw e;
     }
+}
     
 function menu(){
-    alert('menu'); 
+    location.href="list.html"
     }
+    
+function getMemoList() {
+    var list = localStorage.getItem("memo_list");
+    if (list == null) {
+        return new Array();
+    } else {
+        return JSON.parse(list);
+    }
+}
